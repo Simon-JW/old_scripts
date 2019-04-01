91715cedda47d9829f2a5ba31ba9c6a51c312b50
Simon Walker <simon.walker@anu.edu.au>
2019-04-01 20:35:05 +1100

    Moving all algorithm scripts to new folder.

---
+++ gdal_rasters.sh
@@ -0,0 +1,8 @@
+module load python3/3.5.2
+module load python3/3.5.2-matplotlib
+module load ipython/4.2.0-py3.5
+module load gdal-python/1.11.1-py3.5
+module load xarray/0.7.2-py3.5
+
+jupyter notebook
+
